1a3889869ea84bbce4a089607ad3551c8987cec5
Mame Sandeck Niang <niangmamesandeck@gmail.com>
2023-02-25 23:01:39 +0000

    first commit

--- src/main/java/org/msn/App.java
+++ src/main/java/org/msn/App.java
@@ -2,6 +2,6 @@ package org.msn;
 
 public class App {
     public static void main(String[] args){
-        System.out.println("Hello World from Team 4 !");
+        System.out.println("Hello World!");
     }
 }
